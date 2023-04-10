33e7a08ad7f76861016bda027e19989910e52000
ordenmeny <hogvarts06@mail.ru>
2023-04-10 20:03:34 +0500

    sidebars

--- clubs_app/urls.py
+++ clubs_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HomePage, CreateClubView
+from .views import HomePage, CreateClubView, DashboardView
 
 app_name = 'clubs_app'
 
 urlpatterns = [
     path('', HomePage.as_view(), name='home_page'),
-    path('create_club/', CreateClubView.as_view(), name='create_club')
+    path('create_club/', CreateClubView.as_view(), name='create_club'),
+    path('dashboard/', DashboardView.as_view(), name='dashboard'),
 ]
--- clubs_app/views.py
+++ clubs_app/views.py
@@ -1,18 +1,26 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, CreateView
+from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
 from .models import ClubModel
 from .forms import CreateClubForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
 
 class HomePage(TemplateView):
     template_name = 'clubs_app/index.html'
 
 
-class CreateClubView(CreateView):
+class CreateClubView(LoginRequiredMixin, CreateView):
     model = ClubModel
     template_name = 'clubs_app/create_club.html'
     form_class = CreateClubForm
+    success_url = reverse_lazy('clubs_app:home_page')
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+class DashboardView(TemplateView):
+    template_name = 'clubs_app/dashboard.html'
