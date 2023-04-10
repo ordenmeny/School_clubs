81bb6feb6bae411b10398144e3a3a826151e4fcd
ordenmeny <hogvarts06@mail.ru>
2023-04-10 16:16:30 +0500

    create_club:added input fields and styles

--- clubs_app/forms.py
+++ clubs_app/forms.py
@@ -5,9 +5,18 @@ from .models import ClubModel
 class CreateClubForm(forms.ModelForm):
     class Meta:
         model = ClubModel
-        fields = ('name_club', 'author')
+        fields = ('name_club', 'type_club', 'paid_free', 'info_club')
 
         widgets = {
-            'name_club': forms.TextInput(attrs={'class': 'form-control'}),
-            'author': forms.Select(attrs={'class': 'form-control'})
+            'name_club': forms.TextInput(attrs={'class': 'form-control style-placeholder',
+                                                'style': 'height:70px; border-radius:20px; font-size:30px; font-weight:400',
+                                                'placeholder': 'Название клуба:'}),
+            'type_club': forms.Select(attrs={'class': 'form-control form-select',
+                                             'style': 'height:70px; border-radius:20px;font-size:30px; font-weight:400'}),
+            'paid_free': forms.Select(attrs={'class': 'form-control form-select',
+                                             'style': 'height:70px; border-radius:20px;font-size:30px; font-weight:400'}),
+
+            'info_club': forms.Textarea(
+                attrs={'class': 'form-control', 'style': 'border-radius: 20px !important; font-size:30px; font-weight:400;',
+                       'rows': 3, 'placeholder': 'Добавь информацию о клубе:'})
         }
--- clubs_app/models.py
+++ clubs_app/models.py
@@ -3,8 +3,30 @@ from django.conf import settings
 
 
 class ClubModel(models.Model):
-    name_club = models.CharField(max_length=30)
+    types_of_clubs = [
+        ("Тип_не_выбран", "Тип клуба:"),
+        ("IT", "IT"),
+        ("Спорт", "Спорт"),
+        ("Литература", "Литература"),
+        ("Рисование", "Рисование"),
+        ("Культура", "Культура"),
+        ("Музыка", "Музыка"),
+        ("Видеоигры", "Видеоигры"),
+        ("Общение", "Общение"),
+        ("Рукоделие", "Рукоделие"),
+    ]
+
+    paid_or_free = [
+        ("Не_выбрано", "Платно или бесплатно:"),
+        ("Платно", "Платно"),
+        ("Бесплатно", "Бесплатно")
+    ]
+
+    name_club = models.CharField(null=True, blank=True, max_length=30)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    type_club = models.CharField(max_length=100, choices=types_of_clubs, default='Тип_не_выбран')
+    paid_free = models.CharField(max_length=100, choices=paid_or_free, default='Не_выбрано')
+    info_club = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.name_club
