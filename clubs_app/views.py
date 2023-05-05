6095bd4f9b00f962c99bcfcf2f308f09e3a49268
ordenmeny <hogvarts06@mail.ru>
2023-05-05 13:11:36 +0500

    add titles to list_club

--- clubs_app/views.py
+++ clubs_app/views.py
@@ -40,16 +40,23 @@ def create_club(request):
 def list_club(request, cat_slug):
     if cat_slug == 'all_clubs':
         club_model = ClubModel.objects.all()
+        cat = None
     else:
-        club_model = ClubModel.objects.filter(cat_club=CatClubModel.objects.get(slug=cat_slug))
+        cm = CatClubModel.objects.get(slug=cat_slug)
+        club_model = ClubModel.objects.filter(cat_club=cm)
+        cat = cm.cat
+
     context = {
         'club_model': club_model,
+        'if_all_clubs': ('all_clubs' if cat_slug == 'all_clubs' else False),
+        'if_category': ('category' if cat_slug not in ('my_clubs', 'all_clubs') else False),
+        'cat': cat,
     }
     return render(request, template_name='clubs_app/list_club.html', context=context)
 
 
 def join_club(request, club_id):
-    club = ClubModel.objects.get(pk=club_id)  # получить экземпляр класса
+    club = ClubModel.objects.get(pk=club_id)
     club.member.add(request.user.id)
 
     messages.success(request, f'Вы вступили в клуб {club.title_club}')
@@ -58,14 +65,10 @@ def join_club(request, club_id):
 
 @login_required
 def my_clubs(request):
-    # нужно сделать вывод всех клубов, в которых user принимает участие: manager или member
-    # club_model = ClubModel.objects.filter(manager=request.user)
-
     club_model = set(ClubModel.objects.filter(Q(manager=request.user) | Q(member=request.user)))
     context = {
         'club_model': club_model,
         'my_clubs': True,
-        # 'my_clubs': ('manager' if ClubModel.manager == request.user else 'member')
     }
 
     return render(request, template_name='clubs_app/list_club.html', context=context)
@@ -95,7 +98,6 @@ def detail_articles(request, slug_article):
 
 
 def show_messages(request, slug_club):
-
     context = {
         'slug_club': slug_club,
         'messages': MessageClub.objects.filter(sender_club=ClubModel.objects.get(slug_club=slug_club))
